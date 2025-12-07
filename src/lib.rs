6f3b4e46d7fb417067d4721d49b8d2ab5621603d
Bas van der Hoek <bas@hoek.com>
2025-12-08 00:45:17 +0100

    Parallelize queries to rekordbox with r2d2 pool

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use objc2::rc::Retained;
 use objc2_foundation::NSString;
 use objc2_itunes_library::{ITLibMediaItem, ITLibrary};
+use regex::{Captures, Regex};
 
 pub struct Music {
   itl: Retained<ITLibrary>,
@@ -73,9 +74,14 @@ impl Song {
   }
 
   pub fn deezer_id(&self) -> Option<&str> {
-    self.path.as_ref().and_then(|path| happer::deezer::parse_filename(path))
+    self.path.as_ref().and_then(|path| parse_filename(path))
       .and_then(|caps| caps.get(4).map(|id| id.as_str()))
   }
+}
+
+pub fn parse_filename(filename: &str) -> Option<Captures<'_>> {
+  let re = Regex::new(r"^(?:(\d+)\.\s)?(.+)\s--\s(.+)?\s\[(\d+)]\.mp3$").unwrap();
+  re.captures(filename)
 }
 
 #[cfg(test)]
@@ -83,7 +89,10 @@ mod tests {
   use super::*;
   use id3rs::ID3rs;
   use rayon::iter::{IntoParallelIterator, ParallelIterator};
-  use std::fs;
+  use std::{env, fs};
+  use dotenvy::dotenv;
+  use r2d2::{Pool, PooledConnection};
+  use r2d2_sqlite::SqliteConnectionManager;
 
   #[test]
   fn test_playlist_items() {
@@ -98,15 +107,77 @@ mod tests {
   }
 
   #[test]
-  fn test_master_db() {
-    let rb = happer::rekordbox::Rekordbox::new("test_master.db").unwrap();
-    let content = rb.with_deezer("918205852").unwrap();
-    assert_eq!(3, content.Rating);
-    assert_eq!("29. 2020 Souls -- Aaaron [918205852].mp3", content.FileNameL);
+  fn test_sqlcipher() {
+    dotenv().ok();
+    let manager = SqliteConnectionManager::file("test_master.db")
+      .with_init(|conn| {
+        let pragma = format!("PRAGMA key = '{}';", env::var("SQLCIPHER_KEY").unwrap());
+        conn.execute_batch(pragma.as_str())
+      });
+
+    let pool = Pool::new(manager).unwrap();
+    let conn = pool.get().unwrap();
+
+    let count: i64 = conn.query_row(
+      "SELECT COUNT(*) FROM djmdContent",
+      [],
+      |row| row.get(0),
+    ).unwrap();
+    assert_eq!(7347, count);
+    let id: String = conn.query_row(
+      "SELECT * FROM djmdContent WHERE FileNameL like ?",
+      [format!("%[{}]%", "918205852")],
+      |row| row.get(0),
+    ).unwrap();
+    assert_eq!("43970339", id);
+
+    let music = Music::default();
+    let items = music.all_items();
+    let songs: Vec<Song> = items.iter().map(|item| item.into()).collect();
+    // songs.into_iter().for_each(|song| {
+    //   process_db(song, pool.get().unwrap());
+    // });
+    songs.into_par_iter().for_each(|song| {
+      process_db(song, pool.get().unwrap());
+    });
   }
+
+  fn process_db(song: Song, conn: PooledConnection<SqliteConnectionManager>) {
+    if let Some(path) = song.path.as_ref() {
+      if let Some(dzid) = song.deezer_id() {
+        let rbid: Result<String, _> = conn.query_row(
+          "SELECT * FROM djmdContent WHERE FileNameL like ?",
+          [format!("%[{}]%", dzid)],
+          |row| row.get(0),
+        );
+        if rbid.is_err() {
+          eprintln!("{:?} with {:?} not found", song.relative_path(), rbid)
+        }
+      }
+      match fs::exists(&path) {
+        Ok(_) => {
+          match ID3rs::read(&path) {
+            Ok(_) => {}
+            Err(_) => eprintln!("Cannot read {}", path),
+          }
+        }
+        Err(_) => eprintln!("{} does not exist", path),
+      };
+    }
+  }
+
+
+  // #[test]
+  // fn test_master_db() {
+  //   let rb = happer::rekordbox::Rekordbox::new("test_master.db").unwrap();
+  //   let content = rb.with_deezer("918205852").unwrap();
+  //   assert_eq!(3, content.Rating);
+  //   assert_eq!("29. 2020 Souls -- Aaaron [918205852].mp3", content.FileNameL);
+  // }
 
   #[test]
   fn test_process_all() {
+    // let rb = Arc::new(RwLock::new(happer::rekordbox::Rekordbox::new("test_master.db").unwrap()));
     let music = Music::default();
     let items = music.all_items();
     let songs: Vec<Song> = items.iter().map(|item| item.into()).collect();
